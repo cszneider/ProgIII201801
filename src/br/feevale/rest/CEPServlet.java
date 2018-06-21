c05b45261a8189b5f46883c85da73d3cbcc61799
cszneider <cszneider@gmail.com>
2018-06-20 21:18:26 -0300

    finalização do servlet de busca de cep

--- src/br/feevale/rest/CEPServlet.java
+++ src/br/feevale/rest/CEPServlet.java
@@ -2,6 +2,9 @@ package br.feevale.rest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,6 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import br.feevale.conexaoSGBD.Conexao;
 
 @WebServlet("/CEPServlet")
 public class CEPServlet extends HttpServlet {
@@ -20,24 +27,46 @@ public class CEPServlet extends HttpServlet {
 	
 	protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 
+		
 		String cep = request.getParameter( "cep" );
 		
-		if( cep == null ) {
-			cep = "Não Informado";
-		}
+		JSONObject result = new JSONObject();
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append( "<html>" );
-		sb.append( "<body>" );
-		sb.append( "<h1>Boa noite Feevale</h1>" );
-		sb.append( "<h2>Cep de busca: " );
-		sb.append( cep );
-		sb.append( "</h2></body>" );
-		sb.append( "</html>" );
+		try {
+			if( cep == null ) {
+				result.put( "status", -1 );
+				result.put( "dsErro", "cep não informado" );
+			} else {
+				Conexao cnx = new Conexao();
 
-		PrintWriter pw = response.getWriter();
-		pw.write( sb.toString() );
-		response.flushBuffer();
+				try {
+					
+					String cmd = "Select * from cidades where cd_cep = ?";
+					PreparedStatement ps = cnx.prepareStatement( cmd );
+					ps.setString( 1, cep );
+					
+					ResultSet rs = ps.executeQuery();
+					if( rs.next() ) {
+						result.put( "status", 0 );
+						result.put( "cdEstado", rs.getString( "cd_estado" ) );
+						result.put( "idCidade", rs.getInt( "id_cidade" ) );
+						result.put( "dsCidade", rs.getString( "ds_cidade" ).trim() );
+					} else {
+						result.put( "status", -2 );
+						result.put( "dsErro", "cep não encontrado" );
+					}
+				} finally {
+					cnx.close();
+				}
+			}
+
+			PrintWriter pw = response.getWriter();
+			pw.write( result.toString() );
+			pw.flush();
+			
+		} catch( Exception e ) {
+			response.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
